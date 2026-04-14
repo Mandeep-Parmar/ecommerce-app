3d31c19f28072b44fc42532cba8aae12a290f5e3
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-04-14 22:06:48 +0530

    implement admin login and authorization middleware using JWT

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,15 +1,18 @@
 import express from "express";
-import {addProduct,
+import {
+  addProduct,
   listProducts,
   removeProduct,
   singleProduct,
 } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
 productRouter.post(
   "/add",
+  adminAuth,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
@@ -20,7 +23,7 @@ productRouter.post(
 );
 
 productRouter.get("/list", listProducts);
-productRouter.post("/remove", removeProduct);
+productRouter.post("/remove", adminAuth, removeProduct);
 productRouter.post("/single", singleProduct);
 
 export default productRouter;
