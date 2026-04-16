f95ece4f81e05bd1f457a5970a2bd5981044e019
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-04-16 18:06:12 +0530

    integrate frontend with backend and display products dynamically

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -10,7 +10,7 @@ const LatestCollection = () => {
   // change only one time (at render)
   useEffect(() => {
     setLatestProducts(products.slice(0, 10));
-  }, []);
+  }, [products]);
 
   return (
     <div className="my-10">
