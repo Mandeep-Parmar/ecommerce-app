36e41de23d61b40ab36c565d4cbbe2ef95e56380
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-04-16 09:15:40 +0530

    add admin dashboard layout with sidebar and navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <div>
+      <>
+        <Navbar />
+        <hr className="text-gray-400" />
+      </>
+      <div className="flex w-full">
+        <Sidebar />
+      </div>
+    </div>
   );
 };
 
