b83010274f9ea2e6955367ef82c3413f9fe27920
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-04-17 08:09:39 +0530

    add order schema and setup order routes and controllers

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,40 @@
+
+
+// Placing order using COD method
+const placeOrder = async (req, res) => {
+
+};
+
+// Placing order using Stripe method
+const placeOrderStripe = async (req, res) => {
+
+};
+
+// Placing order using Razorpay method
+const placeOrderRazorpay = async (req, res) => {
+
+};
+
+// All orders data for Admin panel
+const allOrders = async (req, res) => {
+
+};
+
+// All User's orders data for Frontend
+const userOrders = async (req, res) => {
+
+};
+
+// Update order status from Admin panel
+const updateStatus = async (req, res) => {
+
+};
+
+export {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+};
