08a49709fb9cee40d5accfd2a3eccd2c07bf715d
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-04-24 14:07:33 +0530

    correct component import path

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
-import NewsLatterBox from "../components/NewsLatterBox";
+import NewsLatterBox from "../components/NewslatterBox";
 
 const About = () => {
   return (
