52c611384b9b56f922dcc618728c4b4297ab756d
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-04-29 19:21:49 +0530

    remove changes

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,15 +1,8 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
-
-// Force Node.js to prefer IPv4 over IPv6. 
-// This fixes the 'ENETUNREACH' error when the server tries to connect to Gmail's IPv6 address but doesn't have IPv6 internet access.
-dns.setDefaultResultOrder("ipv4first");
 
 const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS, // App password
